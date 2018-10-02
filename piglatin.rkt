b313a5ecdbdb59ee0d86d3068ea81aee5e653851
SYZYGY-DEV333 <pipeorgan101@gmail.com>
2018-10-02 16:44:02 -0400

    Create piglatin.rkt

---
+++ piglatin.rkt
@@ -0,0 +1,28 @@
+(define lst '("sh" "gl" "ch" "ph" "tr" "br" "fr" "bl" "gr" "st" "sl" "sl" "pl" "fl" "th"))
+
+(define (flatten list)
+    (cond ((null? list) '())
+        [(list? (car list)) (append (flatten (car list)) (flatten (cdr list)))]
+        [else (cons (car list) (flatten (cdr list)))]))
+
+(define (member? str list) (if [member str list] #t #f))
+
+(define (piglatinize word)
+    (let [(i (string->list word))]
+        (cond 
+            [(member? (car i) '(#\a #\e #\i #\o #\u))
+                (string-append word "ay")]
+            [(member? (string-append (list->string (flatten (list (car i) (cdr i))))) lst)
+                (string-append (list->string (flatten (list (drop i 2)) (list->string (take i 2)))) "ay")]
+            [(not (regexp-match #rx"^[A-Za-z]+$" word))
+                word]
+            [else (string-append (list->string (flatten (list (drop i 1) (car i)))) "ay")])))
+
+(define (piglatin phrase)
+    (string-join (map piglatinize (string-split phrase))))
+
+(define (main)
+    (display "Type what you would like translated into pig-latin and press ENTER: ")
+    (displayln (piglatin (read-line))))
+
+(main)
